286563719eacf4f587aff447d21e19d6d48de9b7
Shravan6488 <32151014+Shravan6488@users.noreply.github.com>
2019-02-24 09:49:11 +0800

    Update static-ips.tf

--- my-cluster/static-ips.tf
+++ my-cluster/static-ips.tf
@@ -1,7 +1,4 @@
 resource "google_compute_global_address" "raddit_static_ip" {
-  name = "raddit-static-ip"
+  name = "shravan-static-ip"
 }
 
-resource "google_compute_address" "gitlab_static_ip" {
-  name = "gitlab-static-ip"
-}
